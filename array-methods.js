a1ecb7504852beca1552a7a1c1f947bc225f1295
Heraniah <brianheraniah27@gmail>
2023-02-10 12:22:46 +0300

    array-methods.js

--- array-methods.js
+++ array-methods.js
@@ -1,23 +1,25 @@
-// ARRAY METHODS
-let colors = ["red", "green", "blue", "yellow", "black", "brown"];
+// // ARRAY METHODS
+// let colors = ["red", "green", "blue", "yellow", "black", "brown"];
 
-let students = [
-  { name: "albert", age: 36 },
-  { name: "brian", age: 22 },
-];
+// let students = [
+//   { name: "albert", age: 36 },
+//   { name: "brian", age: 22 },
+// ];
 
-let matrix = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-];
-// zero indexed
-// console.log(colors[0]); //red
-// console.log(colors[4]); //cyan
+// let matrix = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
+// // zero indexed
+// // console.log(colors[0]); //red
+// // console.log(colors[4]); //cyan
 
-// 8 in matix
-console.log(matrix[2][1]);
-// brian's age
-console.log(students[1].age);
+// // 8 in matix
+// console.log(matrix[2][1]);
+// // brian's age
+// console.log(students[1].age);
 
-console.log("1" * 1);
+// console.log("1" * 1);
+
+let years = [2015, 2016, 2017, 2018];
